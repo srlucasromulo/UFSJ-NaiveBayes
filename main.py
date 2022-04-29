73f0033def51e4fee75c30c70724cefc250118a1
srlucasromulo <lucas.romulo.sr@gmail.com>
2022-04-28 23:33:44 -0300

    calc ok; TODO: check result = result_entry; m-estimate

--- main.py
+++ main.py
@@ -4,40 +4,36 @@ import pandas as pd
 df = pd.read_csv('dataset.csv')
 entry = pd.read_csv('entry.csv')
 # rows, columns = df.shape
-index = df.columns
+columns = df.columns
 
 
 if __name__ == '__main__':
-
-	count_x = {i: None for i in index[1:]}
-	for i in index[1:]:
+	count_x = {i: None for i in columns[1:]}
+	for i in columns[1:]:
 		count_x[i] = df[i].value_counts().to_dict()
-	count_y = df[index[0]].value_counts().to_dict()
 
 	# class probability
-	probabilities_y = df[index[0]].value_counts(normalize=True).to_dict()
+	probabilities_y = df[columns[0]].value_counts(normalize=True).to_dict()
 
 	# xij probability
-	probabilities_x = {i: {} for i in index[1:]}
-	for i in index[1:]:
-		for key in count_x[i].keys():
-			probabilities_x[i].update({key: {}})
-			for y in count_y.keys():
-				xij = df.loc[(df[index[0]] == y) & (df[i] == key)]
-				prop = len(xij) / count_y[y]
-				probabilities_x[i][key].update({y: prop})
+	probabilities_x = {i: {} for i in columns[1:]}
+	for i in columns[1:]:
+		for j in count_x[i].keys():
+			probabilities_x[i].update({j: {}})
+			for y in probabilities_y.keys():
+				xij = df.loc[(df[columns[0]] == y) & (df[i] == j)]
+				prop = len(xij) / count_x[i][j]
+				probabilities_x[i][j].update({y: prop})
 
 	# classify entries
 	for row in entry.iloc:
-		# result_entry = row[index[0]]
+		# result_entry = row[columns[0]]
 		result = {}
-		for y in count_y.keys():
+		for y in probabilities_y.keys():
 			result.update({y: None})
-			calc = 1
-			for i in index[1:]:
+			calc = probabilities_y[y]
+			for i in columns[1:]:
 				calc *= probabilities_x[i][row[i]][y]
-				print(row[i], y, calc)	# DBG check calc
-			print()	# DBG
 			result[y] = calc
 		print(result)
 
